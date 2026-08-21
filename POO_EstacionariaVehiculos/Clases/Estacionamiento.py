be065b9ff75854b2f2d4332d07b85c76a79c44fb
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-21 02:40:00 -0500

    Agrega propiedades y método para verificar capacidad en Estacionamiento
    Se añaden propiedades para acceder a los atributos de la clase Estacionamiento,
    así como un método que verifica si el estacionamiento está lleno.
    Además, se actualizan los tipos de algunos atributos para mejorar la precisión
    en el manejo de datos.

--- POO_EstacionariaVehiculos/Clases/Estacionamiento.py
+++ POO_EstacionariaVehiculos/Clases/Estacionamiento.py
@@ -1,15 +1,37 @@
-﻿from POO_EstacionariaVehiculos.Clases import Coche
+﻿from POO_EstacionariaVehiculos.Clases.Coche import Coche # importando la clase Coche
 
 class Estacionamiento():
 	_nombre : str
 	_capacidad_Max : str
-	_tarifaFija : int
-	_coches_Dentro : list
-	_total : int
+	_tarifaFija : float
+	_coches_Dentro : list[Coche]
+	_total : float
 
 	def __init__(self, nombre : str, capacidad_Max : int, tarifaFija : int) -> None:
 		self._nombre : str = nombre
 		self._capacidad_Max : int = capacidad_Max
-		self._tarifaFija : int = tarifaFija
-		self._coches_Dentro : list = []
-		self._total : int = 0
+		self._tarifaFija : float = tarifaFija
+		self._coches_Dentro : list[Coche] = list()
+		self._total : float = 0.0
+
+	@property
+	def nombre(self) -> str:
+		return self._nombre
+
+	@property
+	def capacidad_Max(self) -> int:
+		return self._capacidad_Max
+
+	@property
+	def tarifaFija(self) -> float:
+		return self._tarifaFija
+
+	@property
+	def totalRecaudado(self) -> float:
+		return self._total
+
+	def esta_lleno(self) -> bool:
+		if(len(self._coches_Dentro) == self._capacidad_Max):
+			return True
+		else:
+			return False
