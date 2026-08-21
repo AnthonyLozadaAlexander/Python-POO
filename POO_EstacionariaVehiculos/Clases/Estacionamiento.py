68c8ae9abca507e35cf34baa5968053dcfc20b1b
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-21 02:11:19 -0500

    Agrega clase Estacionamiento con atributos y constructor

---
+++ POO_EstacionariaVehiculos/Clases/Estacionamiento.py
@@ -0,0 +1,15 @@
+﻿from POO_EstacionariaVehiculos.Clases import Coche
+
+class Estacionamiento():
+	_nombre : str
+	_capacidad_Max : str
+	_tarifaFija : int
+	_coches_Dentro : list
+	_total : int
+
+	def __init__(self, nombre : str, capacidad_Max : int, tarifaFija : int) -> None:
+		self._nombre : str = nombre
+		self._capacidad_Max : int = capacidad_Max
+		self._tarifaFija : int = tarifaFija
+		self._coches_Dentro : list = []
+		self._total : int = 0
