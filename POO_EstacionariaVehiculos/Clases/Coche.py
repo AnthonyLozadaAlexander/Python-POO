77d2255a70aeb73919a6c64319307474196a4608
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-21 00:43:37 -0500

    Agrega clase Coche con atributos y métodos básicos
    Se implementa la clase Coche que incluye atributos como largo, ancho y número de ruedas.
    Además, se añaden métodos para obtener información del coche y un setter para el estado de marcha.

---
+++ POO_EstacionariaVehiculos/Clases/Coche.py
@@ -0,0 +1,43 @@
+﻿class Coche():
+	# Atributos de la clase Coche
+	_largoChasis: int = 0
+	_anchoChasis: int = 0
+	_ruedas: int = 0
+	_enmarcha: bool = False
+
+	# Constructor con parametros
+	def __init__(self, largoChasis, anchoChasis, ruedas):
+		self._largoChasis  = largoChasis
+		self._anchoChasis = anchoChasis
+		self._ruedas = ruedas
+
+	def Coche(self):
+		pass
+
+	@property
+	def largoChasis(self) -> int:
+		return self._largoChasis
+
+	@property
+	def anchoChasis(self) -> int:
+		return self._anchoChasis
+
+	@property
+	def ruedas(self) -> int:
+		return self._ruedas
+
+	@property
+	def enmarcha(self) -> bool:
+		return self._enmarcha
+
+	# Para crear el setter debo crear antes el getter, ya que el setter es un metodo que permite modificar el valor de un atributo privado, en este caso el atributo privado es _enmarcha
+	@enmarcha.setter
+	def enmarcha(self, valor: bool):
+		self._enmarcha = valor
+
+	# Metodo para mostrar la informacion del coche
+	def toString(self) -> str:
+		return (f"Informacion Del Vehiculo: \n"
+		        f" Largo Chasis: {self._largoChasis} \n"
+		        f"Ancho Chasis: {self._anchoChasis} \n"
+		        f"Ruedas: {self._ruedas}")
