675a2e5f81d4a3201589b965f443529b76c3a543
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-22 01:08:57 -0500

    Agrega archivos de configuración para Discord y mejora la clase Estacionamiento con métodos y propiedades

--- POO_EstacionariaVehiculos/Clases/Estacionamiento.py
+++ POO_EstacionariaVehiculos/Clases/Estacionamiento.py
@@ -1,45 +1,48 @@
-﻿from POO_EstacionariaVehiculos.Clases.Coche import Coche # importando la clase Coche
+﻿from POO_EstacionariaVehiculos.Clases.Coche import Coche  # importando la clase Coche
 
-class Estacionamiento():
-	_nombre : str
-	_capacidad_Max : str
-	_tarifaFija : float
-	_coches_Dentro : list[Coche]
-	_total : float
 
-	def __init__(self, nombre : str, capacidad_Max : int, tarifaFija : int) -> None:
-		self._nombre : str = nombre
-		self._capacidad_Max : int = capacidad_Max
-		self._tarifaFija : float = tarifaFija
-		self._coches_Dentro : list[Coche] = [] # se inicializa la lista de coches como vacia
-		self._total : float = 0.0
+class Estacionamiento:
+    _nombre: str
+    _capacidad_Max: int
+    _tarifaFija: float
+    _coches_Dentro: list[Coche]
+    _total: float
 
-	@property
-	def nombre(self) -> str:
-		return self._nombre
+    def __init__(self, nombre: str, capacidad_Max: int, tarifaFija: int) -> None:
+        self._nombre: str = nombre
+        self._capacidad_Max: int = capacidad_Max
+        self._tarifaFija: float = tarifaFija
+        self._coches_Dentro: list[Coche] = (
+            []
+        )  # se inicializa la lista de coches como vacia
+        self._total: float = 0.0
 
-	@property
-	def capacidad_Max(self) -> int:
-		return self._capacidad_Max
+    @property
+    def nombre(self) -> str:
+        return self._nombre
 
-	@property
-	def tarifaFija(self) -> float:
-		return self._tarifaFija
+    @property
+    def capacidad_Max(self) -> int:
+        return self._capacidad_Max
 
-	@property
-	def totalRecaudado(self) -> float:
-		return self._total
+    @property
+    def tarifaFija(self) -> float:
+        return self._tarifaFija
 
-	def esta_lleno(self) -> bool:
-		if(len(self._coches_Dentro) == self._capacidad_Max):
-			return True
-		else:
-			return False
+    @property
+    def totalRecaudado(self) -> float:
+        return self._total
 
-	def buscar_coche(self, placa: str) -> Coche | None:
-		# Bucle foreach que recorre la lista de coches dentro del estacionamiento y busca el coche
-		for coche in self._coches_Dentro:
-			if(coche.placa == placa):
-				return coche
+    def esta_lleno(self) -> bool:
+        if len(self._coches_Dentro) == self._capacidad_Max:
+            return True
+        else:
+            return False
 
-		return None
+    def buscar_coche(self, placa: str) -> Coche | None:
+        # Bucle foreach que recorre la lista de coches dentro del estacionamiento y busca el coche
+        for coche in self._coches_Dentro:
+            if coche.placa == placa:
+                return coche
+
+        return None
